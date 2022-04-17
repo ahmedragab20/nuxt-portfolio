04325cf2b146757c84f4f53bf5b706d10e7f3c16
Ahmed Ragab <ahmedragab20901@gmail.com>
2022-04-17 02:58:28 +0200

    📦 NEW: replace the iPhone project with the Nike project

--- data/projects.js
+++ data/projects.js
@@ -20,19 +20,19 @@ const projects = [
     img: 'https://www.apple.com/v/iphone-13/d/images/overview/cameragallery/memories_hw_static__cusdx5rolvgy_large.jpg',
     url: 'https://ahmedragab20.github.io/iphone12pro/',
   },
-  {
-    id: 2,
-    name: 'ARTV',
-    used: 'Nuxt.js, Vuex, Bootstrap, Sass',
-    img: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/8b445789661413.5dfb9aa4b558d.jpg',
-    url: 'https://ar-tv.vercel.app/',
-  },
+  // {
+  //   id: 2,
+  //   name: 'ARTV',
+  //   used: 'Nuxt.js, Vuex, Bootstrap, Sass',
+  //   img: 'https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/8b445789661413.5dfb9aa4b558d.jpg',
+  //   url: 'https://ar-tv.vercel.app/',
+  // },
   {
     id: 4,
-    name: 'Headphones',
-    used: 'Vue.js, Vuex, Bootstrap, Sass',
-    img: 'https://ahmedragab20.github.io/portofolio_1/img/headphones.02af2289.jpg',
-    url: 'https://ahmedragab20.github.io/vueHeadPhones/',
+    name: 'Nike Shoes',
+    used: 'Nuxt.js, Vuex, Bootstrap, Sass',
+    img: require('@/assets/projects/nike.png'),
+    url: 'https://nuxt-nike.netlify.app/',
   },
 ]
 
